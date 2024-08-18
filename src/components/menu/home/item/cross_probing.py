2d93abccb287974a966dc2d4b4d1036c2117d927
WonwooChoi <59404206+deepwonwoo@users.noreply.github.com>
2024-08-19 00:44:46 +0900

    crossProbing

--- src/components/menu/home/item/cross_probing.py
+++ src/components/menu/home/item/cross_probing.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+import polars as pl
 import re
 import socket
 import dash_mantine_components as dmc
@@ -15,93 +15,98 @@ class CrossProber:
         self.CP = CACHE.get("CP")
         self.CP_socket = self.create_connection() if all(self.CP.values()) else False
         self.connected_btn = self.cp_connected_btn()
-        self.current_view = ""
 
     def create_connection(self):
         try:
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.settimeout(5)  # Default timeout set to 5 seconds
+            client_socket.settimeout(5)
             client_socket.connect((self.CP["host"], self.CP["port"]))
-            print("CP 서버에 성공적으로 연결되었습니다.")
+            logger.info("Successfully connected to CP server.")
             return client_socket
         except (socket.error, socket.timeout) as e:
-            print(f"CP 서버에 연결할 수 없습니다: {e}")
+            logger.error(f"Failed to connect to CP server: {e}")
             return False
 
     def cp_connected_btn(self):
-        return (
-            html.Div(
-                [
-                    dmc.Menu(
-                        [
-                            dmc.MenuTarget(
-                                dmc.Button(
-                                    children=self.CP["tool"],
-                                    variant="light",
-                                    leftSection=get_icon("bx-wifi"),
-                                    rightSection=dmc.SegmentedControl(
-                                        id="cp-object-segmented",
-                                        value="inst",
-                                        color="black",
-                                        data=[
-                                            {"value": "net", "label": "Net"},
-                                            {"value": "inst", "label": "Instance"},
-                                        ],
-                                        size="xs",
-                                        fullWidth=True,
-                                    ),
+        return self.create_connected_button()
+        if self.CP_socket:
+            return self.create_connected_button()
+        else:
+            return self.create_disconnected_button()
+
+    def create_connected_button(self):
+        return html.Div(
+            [
+                dmc.Menu(
+                    [
+                        dmc.MenuTarget(
+                            dmc.Button(
+                                children=self.CP["tool"],
+                                variant="light",
+                                leftSection=get_icon("bx-wifi"),
+                                rightSection=dmc.SegmentedControl(
+                                    id="cp-object-segmented",
+                                    value="inst",
+                                    color="black",
+                                    data=[
+                                        {"value": "net", "label": "Net"},
+                                        {"value": "inst", "label": "Instance"},
+                                    ],
                                     size="xs",
-                                    id="cp-btn",
-                                )
-                            ),
-                            dmc.MenuDropdown(
-                                [
-                                    dmc.MenuLabel(
-                                        dmc.Group(
-                                            [
-                                                dmc.Text("Lib: ", size="xs"),
-                                                dmc.Text(self.CP["lib"], c="red", size="xs"),
-                                            ],
-                                            gap="xs",
-                                        )
-                                    ),
-                                    dmc.MenuLabel(
-                                        dmc.Group(
-                                            [
-                                                dmc.Text("Cell: ", size="xs"),
-                                                dmc.Text(self.CP["cell"], c="red", size="xs"),
-                                            ],
-                                            gap="xs",
-                                        )
-                                    ),
+                                    fullWidth=True,
+                                ),
+                                size="xs",
+                                id="cp-btn",
+                            )
+                        ),
+                        dmc.MenuDropdown(
+                            [
+                                dmc.MenuLabel(
                                     dmc.Group(
                                         [
-                                            dmc.Text("Middle Click", size="lg", fw=700),
-                                            dmc.Text(" to CrossProbe", size="md"),
-                                        ]
-                                    ),
-                                ]
-                            ),
-                        ],
-                        trigger="hover",
-                    ),
-                ]
-            )
-            if self.CP_socket
-            else dmc.Button(
-                "Disabled",
-                leftSection=get_icon("bx-wifi-off"),
-                size="xs",
-                disabled=True,
-                rightSection=dmc.SegmentedControl(
-                    id="cp-object-segmented",
-                    value="",
-                    color="black",
-                    data=[],
-                    size="xs",
-                    fullWidth=True,
+                                            dmc.Text("Lib: ", size="xs"),
+                                            dmc.Text(self.CP["lib"], c="red", size="xs"),
+                                        ],
+                                        gap="xs",
+                                    )
+                                ),
+                                dmc.MenuLabel(
+                                    dmc.Group(
+                                        [
+                                            dmc.Text("Cell: ", size="xs"),
+                                            dmc.Text(self.CP["cell"], c="red", size="xs"),
+                                        ],
+                                        gap="xs",
+                                    )
+                                ),
+                                dmc.Group(
+                                    [
+                                        dmc.Text("Middle Click", size="lg", fw=700),
+                                        dmc.Text(" to CrossProbe", size="md"),
+                                    ]
+                                ),
+                            ]
+                        ),
+                    ],
+                    trigger="hover",
                 ),
-            )
+            ]
+        )
+    
+    def create_disconnected_button(self):
+        return dmc.Button(
+            "Disabled",
+            leftSection=get_icon("bx-wifi-off"),
+            size="xs",
+            disabled=True,
+            rightSection=dmc.SegmentedControl(
+                id="cp-object-segmented",
+                value="",
+                color="black",
+                data=[],
+                size="xs",
+                fullWidth=True,
+            ),
         )
 
     def layout(self):
@@ -120,28 +125,25 @@ class CrossProber:
         pushDesign -top true -hier top1.path1.path2
         selectCurObject -obj net(or inst) -name object_names
         """
-
         try:
             self.CP_socket.sendall(message.encode())
-            logger.debug(f"message: {message}")
-            # response = self.CP_socket.recv(4096)
-            # return response.decode()
+            logger.debug(f"Sent message: {message}")
         except socket.timeout:
-            logger.warning("Timed out to get response")
+            logger.warning("Timed out while sending message")
             raise
         except Exception as e:
-            logger.warning(f"Fail to send message: {e}")
+            logger.error(f"Failed to send message: {e}")
             raise
-        # finally:
-        #     client_socket.close()
+
 
     def close_connection(self):
         if self.CP_socket:
             try:
                 self.CP_socket.close()
             except Exception as e:
-                print(f"Error closing connection: {e}")
+                logger.error(f"Error closing connection: {e}")
 
+    
     def hier_name(self, s, delimiter="."):
         def remove_initial_x(s):
             s = re.sub(r"^[xX]{2}(?!or)", "x", s, flags=re.IGNORECASE)
@@ -161,14 +163,15 @@ class CrossProber:
         try:
             hier_path, name = s.rsplit(delimiter, 1)
         except ValueError:
-            hier_path = ""
-            name = s
+            hier_path, name = "", s
         return hier_path, name
 
     def remove_m(self, obj, selected_name):
         if obj == "inst" and len(selected_name) > 1 and selected_name[0] == "m":
             selected_name = selected_name[1:]
         return selected_name
+
+
 
     def register_callbacks(self, app):
 
@@ -200,77 +203,77 @@ class CrossProber:
             prevent_initial_call=True,
         )
         def get_selected_row(selected_rows, event, obj):
-
-            if obj and event.get("button") == 1 and len(selected_rows) == 1:
-                logger.debug(f"selected_rows: {selected_rows}")
-                selected_row = selected_rows[0]
-                value = event.get("srcElement.innerText")
-                colId = event.get("srcElement.attributes.col-id.nodeValue")
-                request = CACHE.get("REQUEST")
-                groupBy = [col["id"] for col in request.get("rowGroupCols", [])]
-
-                selected_hier_path, selected_name = self.hier_name(value)
-                selected_name = self.remove_m(obj, selected_name)
-
-                # Group CrossProbing
-                if groupBy:
-                    dff = DATAFRAME["df"]
-                    for gc in groupBy:
-                        dff = dff.filter(pl.col(gc) == selected_row[gc])
-
-                    names = set([selected_name])
-                    for group_value in dff[colId]:
-                        group_hier_path, group_hier_name = self.hier_name(group_value)
-                        if group_hier_path == selected_hier_path:
-                            names.add(self.remove_m(obj, group_hier_name))
-
-                        elif group_hier_path.startswith(selected_hier_path):
-                            group_hier_path = group_hier_path.replace(selected_hier_path, "")
-                            if len(group_hier_path) and group_hier_path[0] == ".":
-                                names.add(self.remove_m(obj, group_hier_path.split(".")[1]))
-
-                    if self.current_view == selected_hier_path:
-                        # Single Instance CrossProbing
-                        msg = f"selectCurObject -obj {obj} -name {','.join(names)}\n"
-                    else:
-                        msg = f"select -obj {obj} -hier {selected_hier_path} -name {','.join(names)}\n"
-                else:
-                    # if save view hierarchy
-                    if self.current_view == selected_hier_path:
-                        # Single Instance CrossProbing
-                        msg = f"selectCurObject -obj {obj} -name {selected_name}\n"
-                    else:
-                        # Single Instance CrossProbing
-                        tmsg = (
-                            f"-hier {selected_hier_path} -name {selected_name}"
-                            if selected_hier_path
-                            else f"-name {selected_name}"
-                        )
-                        msg = f"select -obj {obj} {tmsg}\n"
-                        self.current_view = selected_hier_path
-
-                self.send_message(msg)
-
-                noti = dmc.Notification(
-                    title="CrossProbing",
-                    id=f"noti-{msg}",
-                    color="green",
-                    action="show",
-                    message=f"CP command: {msg}",
-                    icon=html.Img(src="assets/icons/bx-send.png"),
-                    style={
-                        "position": "fixed",
-                        "bottom": 70,
-                        "right": 25,
-                        "width": 400,
-                    },
-                )
-                return noti, no_update, []
-
-            # elif event["ctrlKey"]:
-            #    ...
-            # elif selected_rows[0].get("waiver"):
-            #    ...
-
-            else:
+            if not (obj and event.get("button") == 1 and len(selected_rows) == 1):
                 raise exceptions.PreventUpdate
+            
+            selected_row = selected_rows[0]
+            value = event.get("srcElement.innerText")
+            colId = event.get("srcElement.attributes.col-id.nodeValue")
+            request = CACHE.get("REQUEST")
+            groupBy = [col["id"] for col in request.get("rowGroupCols", [])]
+
+            selected_hier_path, selected_name = self.hier_name(value)
+            selected_name = self.remove_m(obj, selected_name)
+
+            # Group CrossProbing
+            if groupBy:
+                dff = DATAFRAME["df"]
+                for gc in groupBy:
+                    dff = dff.filter(pl.col(gc) == selected_row[gc])
+
+                names = set([selected_name])
+                for group_value in dff[colId]:
+                    group_hier_path, group_hier_name = self.hier_name(group_value)
+                    if group_hier_path == selected_hier_path:
+                        names.add(self.remove_m(obj, group_hier_name))
+
+                    elif group_hier_path.startswith(selected_hier_path):
+                        group_hier_path = group_hier_path.replace(selected_hier_path, "")
+                        if len(group_hier_path) and group_hier_path[0] == ".":
+                            names.add(self.remove_m(obj, group_hier_path.split(".")[1]))
+
+                if self.current_view == selected_hier_path:
+                    # Single Instance CrossProbing
+                    msg = f"selectCurObject -obj {obj} -name {','.join(names)}\n"
+                else:
+                    msg = f"select -obj {obj} -hier {selected_hier_path} -name {','.join(names)}\n"
+            else:
+                # if save view hierarchy
+                if self.current_view == selected_hier_path:
+                    # Single Instance CrossProbing
+                    msg = f"selectCurObject -obj {obj} -name {selected_name}\n"
+                else:
+                    # Single Instance CrossProbing
+                    tmsg = (
+                        f"-hier {selected_hier_path} -name {selected_name}"
+                        if selected_hier_path
+                        else f"-name {selected_name}"
+                    )
+                    msg = f"select -obj {obj} {tmsg}\n"
+                    self.current_view = selected_hier_path
+
+            self.send_message(msg)
+
+            noti = dmc.Notification(
+                title="CrossProbing",
+                id=f"noti-{msg}",
+                color="green",
+                action="show",
+                message=f"CP command: {msg}",
+                icon=html.Img(src="assets/icons/bx-send.png"),
+                style={
+                    "position": "fixed",
+                    "bottom": 70,
+                    "right": 25,
+                    "width": 400,
+                },
+            )
+            return noti, no_update, []
+
+        # elif event["ctrlKey"]:
+        #    ...
+        # elif selected_rows[0].get("waiver"):
+        #    ...
+
+        else:
+            raise exceptions.PreventUpdate
